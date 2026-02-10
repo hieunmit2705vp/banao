de7b782f2ab33063f68c9d62350695488ad22713
hieunmit2705vp <hieuk5vip123@gmail.com>
2026-02-11 01:25:43 +0800

    backend init

---
+++ backend/src/main/java/backend/duan/banao/BanaoApplication.java
@@ -0,0 +1,13 @@
+package backend.duan.banao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanaoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/backend/duan/banao/BanaoApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.duan.banao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
