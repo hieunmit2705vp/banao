89ef719e4a18842f6175a5d15e4d9591b7ec62fb
Nguyen Thi Hoa <hoangyn210303>
2026-02-25 23:21:57 +0700

    [BE] Update Quan ly nhan vien -https://hoant210303.atlassian.net/browse/SCRUM-89

---
+++ backend/src/main/java/backend/duan/banao/dto/request/EmployeePasswordUpdateRequest.java
@@ -0,0 +1,10 @@
+package backend.duan.banao.dto.request;
+
+import lombok.Data;
+
+@Data
+public class EmployeePasswordUpdateRequest {
+    private String newPassword;
+    private String confirmPassword;
+}
+
---
+++ backend/src/main/java/backend/duan/banao/repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package backend.duan.banao.repositories;
+
+import backend.duan.banao.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+}
